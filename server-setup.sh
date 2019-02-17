80fc6f271ad2e363ab1a6ffd5cd1dd606d2bdf16
Zac Littleberry <zaclittleberry@gmail.com>
2019-02-17 14:56:31 -0500

    first commit

---
+++ server-setup.sh
@@ -0,0 +1,7 @@
+# before you can do this, you will need git to easily get this repo
+# run:
+# sudo apt install git
+
+# digital ocean has a droplet with docker and docker-compose installed
+# if you aren't using that, you will need to install them as well
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04
